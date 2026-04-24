6f5cbce7ec8fa7e3392be3cb55ab9ec8ddc7b6ea
Sherry's Laptop <slins1113@gmail.com>
2026-04-24 14:42:56 -0400

    Add Text

--- Collaboration/ContentView.swift
+++ Collaboration/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Text("hello world")
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
